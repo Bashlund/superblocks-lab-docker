92e85016603c79ef20a32ef935bfa15e78845304
Thomas Backlund <thomas@superblocks.com>
2018-09-21 09:56:12 +0200

    Remove GETH_RUN since it is moved to it's own module

--- Spacefile.sh
+++ Spacefile.sh
@@ -102,16 +102,3 @@ MAKE()
     cd "${home}"
     make "$@"
 }
-
-GETH_RUN()
-{
-    SPACE_SIGNATURE="address value"
-
-    local addresses="${1}"
-    shift
-
-    local value="${1}"
-    shift
-
-    docker run -it --entrypoint="sh" -p 8545:8545 ethereum/client-go:alpine -c "(echo \"var addresses='$addresses'; addresses.split(',').map(function(address){ eth.sendTransaction({from:eth.accounts[0], to:address, value:${value}})});\"; cat) | /geth --dev --mine console --rpc --rpcaddr 0.0.0.0 --rpccorsdomain='*'"
-}
